1fa3fd84b4f1114cc3de40e1a12759fe72f2f66d
sealocal <local.mat@gmail.com>
2014-01-14 20:38:38 -0800

    Add a welcome page test. Fixes #8.

--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -4,6 +4,7 @@ feature "WelcomPage" do
   scenario "displays Welcome message" do
     visit root_path
     page.must_have_content "Welcome to my portfolio app!"
+    page.must_have_content "Hire"
     page.wont_have_content "Welcome aboard"
   end
 end
