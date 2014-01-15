9dba35dd2001448a120315f30762c33abdfcc99c
sealocal <local.mat@gmail.com>
2014-01-15 14:04:14 -0800

    update tests. Fixes #8.

--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
-feature "WelcomPage" do
+feature "WelcomePage" do
   scenario "displays Welcome message" do
     visit root_path
     page.must_have_content "Welcome to my portfolio app!"
     page.must_have_content "Hire"
     page.wont_have_content "Welcome aboard"
+    page.wont_have_content "Leave me alone!"
   end
 end
