dc2d1b6d8c009418bb039bdd04a647a60b346a59
sealocal <local.mat@gmail.com>
2014-01-16 10:03:33 -0800

    modify Delete post test

--- test/features/deleting_a_post_test.rb
+++ test/features/deleting_a_post_test.rb
@@ -3,10 +3,9 @@ require "test_helper"
 feature "Deleting a Post" do
   scenario "post is deleted with a click" do
     #Given an existing post
-    post = Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
     visit posts_path
     #When the delete link is clicked
-    page.find("tr#post_#{posts(:cr).id}").click_on "Destroy"
+    page.find("tbody tr:last").click_on "Destroy"
     #Then the post is deleted
     page.wont_have_content "Code Rails"
     page.wont_have_content "This is how I learned web development!"
