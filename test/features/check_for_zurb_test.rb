cba944973e53a02492e8c81b4d6ec5863e3777af
sealocal <local.mat@gmail.com>
2014-01-18 12:12:30 -0800

    update minitest features

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-feature "Check for Zurb" do
+feature "Zurb Foundation" do
   scenario "found columns on the home page" do
     visit root_path
-    page.must_have_content '<meta class=\"foundation-mq-'
+    page.must_have_selector 'section.top-bar-section'
   end
 end
--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -3,9 +3,8 @@ require "test_helper"
 feature "WelcomePage" do
   scenario "displays Welcome message" do
     visit root_path
-    page.must_have_content "Welcome to my portfolio app!"
-    page.must_have_content "Hire"
-    page.wont_have_content "Welcome aboard"
+    page.must_have_content "Minimal Design"
+    page.must_have_content "Lorem ipsum"
     page.wont_have_content "Leave me alone!"
   end
 end
