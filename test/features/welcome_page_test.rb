a26420f980e80eee8ec1c20322dc2bd2ca10c038
sealocal <local.mat@gmail.com>
2014-01-19 15:56:14 -0800

    modify background color and image

--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "WelcomePage" do
   scenario "displays Welcome message" do
-    visit root_path
+    visit http://mikes-cf-portfolio.herokuapp.com/
     page.must_have_content "Minimal Design"
     page.must_have_content "Lorem ipsum"
     page.wont_have_content "Leave me alone!"
