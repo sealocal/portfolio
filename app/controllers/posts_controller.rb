be34cb439c76b643fb306125b909823e8d3ed567
sealocal <local.mat@gmail.com>
2014-01-27 22:45:35 -0800

    add conditional parameter of published to params whitelist

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, (:published if current_user.role == "editor"))
     end
 end
