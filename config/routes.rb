8892ea2454248228107f5c0b74648e864798a854
sealocal <local.mat@gmail.com>
2014-01-29 15:44:28 -0800

    Add comments resource and test feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Portfolio::Application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :posts
 
---
+++ test/features/comments/creating_a_comment_test.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+
+feature "Creating a Comment" do
+  scenario "" do
+  end
+end
