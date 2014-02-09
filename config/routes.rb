1a5f95fc17313ef92dfe504d36be760d396fb7ed
sealocal <local.mat@gmail.com>
2014-02-09 15:01:51 -0800

    update static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Portfolio::Application.routes.draw do
   get '/services' => 'pages#services'
   root 'pages#home'
 
-
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
