418b4d2d0551f7232272774f189c957fe80c281a
sealocal <local.mat@gmail.com>
2014-02-19 14:47:28 -0800

    add image column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218225714) do
+ActiveRecord::Schema.define(version: 20140219214802) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140218225714) do
     t.string   "technologies_used"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
