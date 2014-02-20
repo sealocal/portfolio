76fafb6f708e004d625796a2789f1f399006a91c
sealocal <local.mat@gmail.com>
2014-02-19 22:58:42 -0800

    add worker logic

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,25 @@ class Project < ActiveRecord::Base
   has_many :comments, as: :commentable, dependent: :destroy
 
   mount_uploader :image, ImageUploader
+
+  after_save :enqueue_image
+
+  def image_name
+    File.basename(image.path || image.filename) if image
+  end
+
+  def enqueue_image
+    ImageWorker.perform_async(id, key) if key.present?
+  end
+
+  class ImageWorker
+    include Sidekiq::Worker
+
+    def perform(id, key)
+      project = Project.find(id)
+      project.key = key
+      project.remote_image_url = project.image.direct_fog_url(with_path: true)
+      project.save!
+    end
+  end
 end
