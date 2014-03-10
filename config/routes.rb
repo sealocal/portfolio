48fb111441e425f50064ec6999a37bfd3683985d
sealocal <local.mat@gmail.com>
2014-03-10 16:11:27 -0700

    remove posts and projects routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Portfolio::Application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
-  resources :projects do
-    resources :comments
-  end
+  # resources :projects do
+  #   resources :comments
+  # end
 
-  resources :posts do
-    resources :comments
-  end
+  # resources :posts do
+  #   resources :comments
+  # end
 
   #resources :contact, only: [:new, :create], as: :contact
   match 'contact', via: :get,  to: 'contact#new',     as: 'new_contact'
@@ -18,15 +18,11 @@ Portfolio::Application.routes.draw do
   get '/scratch_space' => 'pages#scratch_space'
   root 'pages#home'
 
-
-
-
   #remove :locale
   # scope ":locale" do
   # end
 
-
-# The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # Example of regular route:
