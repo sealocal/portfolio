f46789202442a637fe137979608839424d0fb873
Mike Taylor <local.mat@gmail.com>
2023-03-12 00:10:16 -0800

    add rewrites for function endpoint

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.region('us-west1').https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
