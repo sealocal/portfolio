985539a5ca93455702d6b0d2c93c64d7a2c6901a
Mike Taylor <local.mat@gmail.com>
2023-03-12 00:46:27 -0800

    add copyright year and date functions

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,24 @@ const functions = require("firebase-functions");
 // https://firebase.google.com/docs/functions/get-started
 
 exports.helloWorld = functions.
-    region("us-west1").https.onRequest((request, response) => {
+    region("us-west1").
+    https.onRequest((request, response) => {
       functions.logger.info("Hello logs!", {structuredData: true});
       response.send("Hello from Firebase!");
+    });
+
+exports.date = functions.
+    region("us-west1").
+    https.onRequest((req, res) => {
+      res.status(200).send(
+          new Date().toString(),
+      );
+    });
+
+exports.getCopyRightSpan = functions.
+    region("us-west1").https.onRequest((req, res) => {
+      const fullYear = new Date().getFullYear();
+      res.status(200).send(`
+        <span>© ${fullYear}</span>
+      `);
     });
