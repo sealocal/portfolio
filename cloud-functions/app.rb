6a837d75238ac0a9adc13629168179e2dd14743f
Mike Taylor <local.mat@gmail.com>
2023-03-13 14:04:20 -0700

    rename executed function handler

--- cloud-functions/app.rb
+++ cloud-functions/app.rb
@@ -1,5 +1,5 @@
 require "functions_framework"
-FunctionsFramework.http "hello_http" do |request|
+FunctionsFramework.http "get_apple_map_service_token_ruby" do |request|
   input = JSON.parse request.body.read rescue {}
   msg = input["message"].to_s
   msg.empty? ? "Hello ruby in GCF 2nd gen!" : msg
