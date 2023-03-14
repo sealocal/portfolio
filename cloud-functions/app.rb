3b6f4330875c0cd92bc41ec78b5baa6a64893305
Mike Taylor <local.mat@gmail.com>
2023-03-14 15:06:07 -0700

    add origin restriction and token response to function

--- cloud-functions/app.rb
+++ cloud-functions/app.rb
@@ -2,5 +2,9 @@ require_relative "lib/apple_map_service"
 
 require "functions_framework"
 FunctionsFramework.http "get_apple_map_service_token_ruby" do |request|
-  AppleMapService::JWT.call
+  response = Rack::Response.new AppleMapService::JWT.call
+  # response.add_header 'Access-Control-Allow-Origin', '*'               # development
+  response.add_header 'Access-Control-Allow-Origin', 'https://miket.dev'  # production
+  response.add_header 'Vary', 'Origin'
+  response
 end
