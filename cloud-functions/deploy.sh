ecf82d3113dc38377a3ce57e36709a5f197b14eb
Mike Taylor <local.mat@gmail.com>
2023-03-16 01:32:06 -0700

    add secrets and env vars to deployment

--- cloud-functions/deploy.sh
+++ cloud-functions/deploy.sh
@@ -1,1 +1,4 @@
-gcloud functions deploy get-apple-map-service-token-ruby --region us-west1 --allow-unauthenticated --source . --trigger-http --runtime ruby32 --gen2 --entry-point get_apple_map_service_token_ruby --memory 128Mi
+gcloud functions deploy get-apple-map-service-token-ruby --region us-west1 --allow-unauthenticated \
+    --source . --trigger-http --runtime ruby32 --gen2 --entry-point get_apple_map_service_token_ruby --memory 128Mi \
+    --set-secrets "/etc/secrets/AuthKey_HL46T79ZJH_p8=AuthKey_HL46T79ZJH_p8:latest" \
+    --env-vars-file .env.yaml
--- cloud-functions/lib/apple_map_service.rb
+++ cloud-functions/lib/apple_map_service.rb
@@ -11,22 +11,29 @@ module AppleMapService
     # Path to private key of KEY_ID (.p8 file downloaded drom Apple Developer Account)
     KEY_PATH = ENV["APPLE_MAP_SERVICE_KEY_PATH"]
     # Read the key from a file path to create an ECDSA key
-    ENCRYPTED_PRIVATE_KEY = OpenSSL::PKey::EC.new(File.read(KEY_PATH))
+    # ? Mounted secrets not availalbe at build time?
+    # ENCRYPTED_PRIVATE_KEY = OpenSSL::PKey::EC.new(File.read(KEY_PATH))
 
     # Heavily inspired by these resources:
     # https://developer.apple.com/forums/thread/707418?answerId=716697022#716697022
     # https://developer.apple.com/forums/thread/707418?answerId=716724022#716724022
     def self.call
+      # Read the key from a file path to create an ECDSA key (at run time)
+      encrypted_private_key = OpenSSL::PKey::EC.new(File.read(KEY_PATH))
+
       # Create a JWT for accessing the Apple Maps Server API (Beta)
       jwt = ::JWT.encode({
         iss: TEAM_ID,
         iat: Time.now.to_i,
         exp: Time.now.to_i + (60 * 2), # expires in 2 minutes
         sub: SERVICE_ID,
-      }, ENCRYPTED_PRIVATE_KEY, 'ES256', {
+      }, encrypted_private_key, 'ES256', {
         kid: KEY_ID,
         id: "#{TEAM_ID}.#{SERVICE_ID}",
       })
+
+      # val = ::JWT.decode jwt, encrypted_private_key, true, { algorithm: 'ES256' }
+      # STDOUT.puts val
 
       jwt
     end
